6ba9821efc6893f2768ce0dc6ea5b50aef967166
venu <venushrinivas123@gmail.com>
2022-02-28 18:42:41 -0500

    tuiter completed

--- public/tuiter/ExploreScreen/ExploreScreen.js
+++ public/tuiter/ExploreScreen/ExploreScreen.js
@@ -1,5 +1,5 @@
 import NavigationSidebar from "../NavigationSidebar";
-//import ExploreComponent from "./ExploreComponent.js";
+import ExploreComponent from "./ExploreComponent.js";
 import WhoToFollowList from "../WhoToFollowList";
 
 (function ($) {
@@ -9,8 +9,8 @@ import WhoToFollowList from "../WhoToFollowList";
             ${NavigationSidebar()}
         </div>
         <div class="col-10 col-xxl-6 col-xl-6 col-lg-7 col-md-10 col-sm-10 wd-mid-content">
+            ${ExploreComponent()}
         </div>
-
         <div class="wd-who-to-follow d-none d-xxl-block d-xl-block d-lg-block col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-4">
             ${WhoToFollowList()}
         </div>
---
+++ public/tuiter/PostSummaryList/posts.js
@@ -0,0 +1,30 @@
+export default [
+    {
+        topic: 'Web Development',
+        userName: 'ReactJS',
+        time: '2h',
+        description: 'React.js is a component based front end library that makes it very easy to build Single Page Applications or SPAs',
+        image: '../assets/images/react.png'
+    },
+    {
+        topic: 'Web Development',
+        userName: 'JavaScript',
+        time: '2h',
+        description: 'JavaScript is a programming language that can run on browsers as well as desktops.',
+        image: '../assets/images/js.png'
+    },
+    {
+        topic: 'Web Development',
+        userName: 'JQuery',
+        time: '1 day',
+        description: 'jQuery is a JavaScript library designed to simplify HTML DOM tree traversal and manipulation, as well as event handling, CSS animation, and Ajax',
+        image: '../assets/images/jquery.png'
+    },
+    {
+        topic: 'Web Development',
+        userName: 'NodeJS',
+        time: '1 week',
+        description: 'Node.js is an open-source, cross-platform, back-end JavaScript runtime environment that runs on the V8 engine and executes JavaScript code outside a web browser.',
+        image: '../assets/images/node.jpeg'
+    },
+]
