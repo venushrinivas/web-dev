ed89068cc42c5d759817faa81f23069416814b3f
venu <venushrinivas123@gmail.com>
2022-03-20 16:48:56 -0400

    Revert "Revert "a6 labs done""
    This reverts commit b1d17e89753d7e0138384699e6393333c8dd863b.

---
+++ src/components/hello-world.js
@@ -0,0 +1,12 @@
+import react from 'react';
+import {Link} from "react-router-dom";
+
+const HelloWorld = () => {
+    return (
+        <>
+            <h1>Hello World!</h1>
+            <Link to="/">Labs</Link> | <Link to="/tuiter">Tuiter</Link>
+        </>
+    )
+};
+export default HelloWorld;
---
+++ src/components/labs/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import Classes from "./classes";
+import Styles from "./styles";
+import ConditionalOutput from "./conditional-output";
+import TodoItem from "./todo/TodoItem";
+import TodoList from "./todo/TodoList";
+
+const Labs = () => {
+    return (
+        <>
+            <h1>Labs</h1>
+            <Link to="/hello">Hello</Link> | <Link to="/tuiter">Tuiter</Link>
+            <ConditionalOutput/>
+            <Styles/>
+            <Classes/>
+            <TodoList/>
+        </>
+    )
+};
+
+export default Labs;
